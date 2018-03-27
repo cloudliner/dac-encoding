82a206cd6d65909917873b846ac969346364cb46
cloudliner <cloudliner.jp@gmail.com>
2018-03-27 21:19:15 +0900

    use template literals

--- src/codepoint.js
+++ src/codepoint.js
@@ -1,8 +1,8 @@
-console.log('A : ', [... 'A'].length, 'CodePoints');
-console.log('1 : ', [... '1'].length, 'CodePoints');
-console.log('Å : ', [... 'Å'].length, 'CodePoints');
-console.log('ԯ : ', [... 'ԯ'].length, 'CodePoints');
-console.log('あ : ', [... 'あ'].length, 'CodePoints');
-console.log('漢 : ', [... '漢'].length, 'CodePoints');
-console.log('𠘨 : ', [... '𠘨'].length, 'CodePoints');
-console.log('🌈 : ', [... '🌈'].length, 'CodePoints');
+console.log(`A : ${ [... 'A'].length } CodePoints`);
+console.log(`1 : ${ [... '1'].length } CodePoints`);
+console.log(`Å : ${ [... 'Å'].length } CodePoints`);
+console.log(`ԯ : ${ [... 'ԯ'].length } CodePoints`);
+console.log(`あ : ${ [... 'あ'].length } CodePoints`);
+console.log(`漢 : ${ [... '漢'].length } CodePoints`);
+console.log(`𠘨 : ${ [... '𠘨'].length } CodePoints`);
+console.log(`🌈 : ${ [... '🌈'].length } CodePoints`);
--- src/utf-8.js
+++ src/utf-8.js
@@ -1,8 +1,8 @@
-console.log('A : ', Buffer.byteLength('A', 'utf8'), 'Bytes');
-console.log('1 : ', Buffer.byteLength('1', 'utf8'), 'Bytes');
-console.log('Å : ', Buffer.byteLength('Å', 'utf8'), 'Bytes');
-console.log('ԯ : ', Buffer.byteLength('ԯ', 'utf8'), 'Bytes');
-console.log('あ : ', Buffer.byteLength('あ', 'utf8'), 'Bytes');
-console.log('漢 : ', Buffer.byteLength('漢', 'utf8'), 'Bytes');
-console.log('𠘨 : ', Buffer.byteLength('𠘨', 'utf8'), 'Bytes');
-console.log('🌈 : ', Buffer.byteLength('🌈', 'utf8'), 'Bytes');
+console.log(`A : ${ Buffer.byteLength('A', 'utf8') } Bytes`);
+console.log(`1 : ${ Buffer.byteLength('1', 'utf8') } Bytes`);
+console.log(`Å : ${ Buffer.byteLength('Å', 'utf8') } Bytes`);
+console.log(`ԯ : ${ Buffer.byteLength('ԯ', 'utf8') } Bytes`);
+console.log(`あ : ${ Buffer.byteLength('あ', 'utf8') } Bytes`);
+console.log(`漢 : ${ Buffer.byteLength('漢', 'utf8') } Bytes`);
+console.log(`𠘨 : ${ Buffer.byteLength('𠘨', 'utf8') } Bytes`);
+console.log(`🌈 : ${ Buffer.byteLength('🌈', 'utf8') } Bytes`);
